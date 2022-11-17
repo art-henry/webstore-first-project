6c8ecdcde81d3ad38ce30a25022dd54bfe54babd
art-henry <ooohenrik@outlook.com>
2022-11-17 14:11:59 +0100

    обновил gulp и зависимости переделал gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-    var gulp         = require('gulp'), // Подключаем Gulp
-    browserSync  = require('browser-sync'), // Подключаем Browser Sync
-    concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-    uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
-    cssnano      = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-    rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
-    imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-    pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-    cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
-    autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
-    spritesmith = require('gulp.spritesmith'), // Подключение библиотеки для создания спрайтов
-    merge = require('merge-stream');
+const    gulp         = require('gulp'); // Подключаем Gulp
+const    browserSync  = require('browser-sync'); // Подключаем Browser Sync
+const    concat       = require('gulp-concat'); // Подключаем gulp-concat (для конкатенации файлов)
+const    uglify       = require('gulp-uglifyjs'); // Подключаем gulp-uglifyjs (для сжатия JS)
+const    cssnano      = require('gulp-cssnano'); // Подключаем пакет для минификации CSS
+const    rename       = require('gulp-rename'); // Подключаем библиотеку для переименования файлов
+const    cache        = require('gulp-cache'); // Подключаем библиотеку кеширования
+const    autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
+const    spritesmith  = require('gulp.spritesmith'); // Подключение библиотеки для создания спрайтов
+const    merge        = require('merge-stream');
 
 gulp.task('css', function(){ // Создаем таск Sass
     return gulp.src('src/css/**/*.css') // Берем источник
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
@@ -48,32 +48,20 @@ gulp.task('scripts', function() {
     return gulp.src('src/js/**/*.js')
         .pipe(concat('plugins.min.js')) // Собираем их в кучу в новом файле plugins.min.js
         .pipe(uglify()) // Сжимаем JS файл
-        .pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+        .pipe(gulp.dest('app/js')) // Выгружаем в папку app/js
+        .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('css-libs', ['css'], function() {
-    return gulp.src('app/css/style.css') // Выбираем файл для минификации
-        .pipe(cssnano()) // Сжимаем
-        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-        .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
-});
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({stream: true}));
+})
 
-gulp.task('watch', ['browser-sync', 'css', 'scripts', 'sprite'], function() {
-    gulp.watch('src/css/**/*.css', ['css']); // Наблюдение за css файлами в папке css
-    gulp.watch('src/sprite/*.png', ['sprite']); // Наблюдение за папкой с картинками для спрайтов  папке sprite
-    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-    gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
-});
-
-gulp.task('img', function() {
-    return gulp.src('src/img/**/*') // Берем все изображения из app
-        .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
-            interlaced: true,
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        })))
-        .pipe(gulp.dest('app/img')); // Выгружаем на продакшен
+gulp.task('watch', function() {
+    gulp.watch('src/css/**/*.css', gulp.parallel('css')); // Наблюдение за css файлами в папке css
+    gulp.watch('src/sprite/*.png', gulp.parallel('sprite')); // Наблюдение за папкой с картинками для спрайтов  папке sprite
+    gulp.watch('app/*.html', gulp.parallel('html')); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('src/js/**/*.js', gulp.parallel('scripts'));   // Наблюдение за JS файлами в папке js
 });
 
 
@@ -81,4 +69,4 @@ gulp.task('clear', function () {
     return cache.clearAll();
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'clear'));
